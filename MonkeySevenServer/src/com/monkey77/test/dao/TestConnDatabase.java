de2e51be13b8381c143784dd7ec360f18f946b8f
SmallMaoBoy <451345057@qq.com>
2015-12-16 00:29:47 +0800

    配置数据库连接信息

---
+++ MonkeySevenServer/src/com/monkey77/test/dao/TestConnDatabase.java
@@ -0,0 +1,13 @@
+package com.monkey77.test.dao;
+
+import org.junit.Test;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class TestConnDatabase extends HibernateDaoSupport{
+	
+	@Test
+	public void testConnDataBase(){
+		//this.getHibernateTemplate()
+	}
+	
+}
